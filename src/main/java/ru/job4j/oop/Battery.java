49ccd3fa772f74dade954285d3c738da0fdb8240
Dm-JVL <JVLEARN@YANDEX.RU>
2020-11-25 17:34:28 +0300

    1.7. Состояние объекта [#374175]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    public void exchange(Battery another) {
+        this.load--;
+        another.load++;
+    }
+
+    public static void main(String[] args) {
+        Battery batteryOne = new Battery(50);
+        Battery batteryTwo = new Battery(75);
+        batteryTwo.exchange(batteryOne);
+        System.out.println("Заряд батареи 1: " + batteryOne.load);
+        System.out.println("Заряд батареи 2: " + batteryTwo.load);
+    }
+}
