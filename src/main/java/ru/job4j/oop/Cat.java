28d57a894acbfc7058aa0cc5441d57d1980883ba
Dm-JVL <JVLEARN@YANDEX.RU>
2020-11-25 15:22:02 +0300

    1.5. Поля объекта [#374170]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,28 @@
 package ru.job4j.oop;
 
 public class Cat {
+    public String nickName, eatFood;
+
+    public void giveNick(String nick) {
+        this.nickName = nick;
+    }
+
+    public void eat(String food) {
+        this.eatFood = food;
+    }
+
+    public void show() {
+        System.out.println("Nickname - " + this.nickName + "\nFood - " + this.eatFood + "\n");
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("black");
+        black.eat("fish");
+        black.show();
     }
 }
