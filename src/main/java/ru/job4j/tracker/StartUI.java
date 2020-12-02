df3939f1f5f9881abf70c33f06384fa6dc2616ed
Dm-JVL <JVLEARN@YANDEX.RU>
2020-12-02 19:54:22 +0300

    2.1. Реализация класса StartUI [#374130]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,10 +3,162 @@ package ru.job4j.tracker;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
+
+    public void init(Tracker tracker) {
+        Scanner scanner = new Scanner(System.in);
+        String buf;
+        boolean exitTrue = false;
+        int select = -1;
+        do {
+            this.showMenu();
+            buf = scanner.nextLine();
+            if (this.isNumber(buf) && (Integer.valueOf(buf) >= 0 && Integer.valueOf(buf) <= 6)){
+                select = Integer.valueOf(buf);
+                exitTrue = this.selectSwitch(select, scanner, tracker);
+            } else{
+                System.out.println("Error - not correct number!\n Please try again!\n");
+            }
+        } while (!exitTrue);
+
+    }
+
+    public int enterId(Scanner scan, Tracker track) {
+        int id = 0;
+        boolean editComp = false;
+        do {
+            String bufId = scan.nextLine();
+            if (this.isNumber(bufId)) {
+                id = Integer.valueOf(bufId);
+                if (track.findById(id) != null) {
+                    return id;
+                } else {
+                    System.out.println("Error! Id not found!\n");
+                    return -1;
+                }
+            } else {
+                System.out.println("Error! Please try again!\n");
+            }
+        } while (true);
+
+    }
+
+    public boolean selectSwitch(int select, Scanner scan, Tracker track) {
+        String name, buf, ItemName;
+        int newId;
+        boolean result = false;
+        Item item;
+        switch (select) {
+            case 0: {
+                System.out.println("=== Create a new Item ====\n");
+                System.out.println("Enter name: \n");
+                name = scan.nextLine();
+                item = new Item(0, name);
+                track.add(item);
+                System.out.println("Application added!\n");
+                break;
+            }
+            case 1: {
+                System.out.println("=== Output all items ====\n");
+                track.showAll(track.findAll());
+                System.out.println("Data output is completed!\n");
+                break;
+            }
+            case 2: {
+                System.out.println("=== Please enter item ID ====\n");
+                newId = enterId(scan, track);
+                System.out.println("=== Please enter item Name ====\n");
+                ItemName = scan.nextLine();
+                item = new Item(0, ItemName);
+                if (track.replace(newId, item)) {
+                    System.out.println("Data edit completed!\n");
+                } else {
+                    System.out.println("Error replace Item!\n");
+                }
+                break;
+            }
+            case 3: {
+                System.out.println("=== Please enter item ID to delete ====\n");
+                newId = enterId(scan, track);
+                if (track.delete(newId)) {
+                    System.out.println("Item deleted!\n");
+                } else {
+                    System.out.println("Error delete Item!\n");
+                }
+
+                break;
+            }
+            case 4: {
+                System.out.println("=== Please enter the search id ====\n");
+                newId = enterId(scan, track);
+                if (newId == -1) {
+                } else {
+                    System.out.println("Item found:\n");
+                    System.out.println(track.findById(newId).toString());
+                }
+                break;
+            }
+            case 5: {
+                System.out.println("=== Please enter the search Item name ====\n");
+                name = scan.nextLine();
+                Item[] rsl = track.findByName(name);
+                if (rsl != null) {
+                    track.showAll(rsl);
+                } else {
+                    System.out.println("Applications with this name were not found!\n");
+                }
+                break;
+            }
+            case 6: {
+                System.out.println("Program completed!\nGood by!");
+                result = true;
+                break;
+            }
+            default: {
+                System.out.println("Error! Please try again!\n");
+                break;
+            }
+        }
+        return result;
+    }
+
+    public boolean isNumber(String s) {
+        try {
+            int i = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void showMenu() {
+        System.out.println(
+                "Menu.\n" +
+                        "0. Add new Item\n" +
+                        "1. Show all items\n" +
+                        "2. Edit item\n" +
+                        "3. Delete item\n" +
+                        "4. Find item by Id\n" +
+                        "5. Find items by name\n" +
+                        "6. Exit Program\n" +
+                        "Select:\n"
+        );
+    }
+
+
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
+        new StartUI().init(tracker);
+    }
+}
+
+
+
+
+    /*public static void main(String[] args) {
+        /*Tracker tracker = new Tracker();
         Item[] item = new Item[4];
         item[0] = new Item(1, "Name_1");
         item[1] = new Item(2, "Name_2");
@@ -26,5 +178,5 @@ public class StartUI {
         String currentDateTimeFormat = currentDateTime.format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
         System.out.println(item.toString());*/
-    }
-}
+
+
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -13,6 +13,14 @@ public class Tracker {
         return item;
     }
 
+    public void showAll(Item [] itemsShow) {
+        for (int i = 0; i < itemsShow.length; i++) {
+            if (itemsShow[i] != null) {
+                System.out.println(itemsShow[i].toString());
+            } else System.out.println("Items №" + (i + 1) + " is empty!");
+        }
+    }
+
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index != -1) {
@@ -57,7 +65,6 @@ public class Tracker {
 
     public Item[] findByName(String key) {
         Item[] rsl = new Item[size];
-
         int k = 0;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
