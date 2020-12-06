2c4508afd64226d15ee055bb9247426d76671427
Dm-JVL <JVLEARN@YANDEX.RU>
2020-12-06 11:33:51 +0300

    Исправление 3.1. Приведение типов. Повышение типа и понижение типа. [#374123]

--- src/main/java/ru/job4j/oop/College.java
+++ src/main/java/ru/job4j/oop/College.java
@@ -4,5 +4,6 @@ public class College {
     public static void main(String[] args) {
         Freshman fresh = new Freshman();
         Student st = fresh; //UpCast!
+        Object ob = fresh;
     }
 }
