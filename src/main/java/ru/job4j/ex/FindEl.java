4dd32d0a79929ea19f5aa0a8e27583b4faf7f19b
Dm-JVL <JVLEARN@YANDEX.RU>
2021-02-15 17:17:24 +0300

    Исправление. 0.4. Пользовательские исключения. [#374140]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -6,6 +6,7 @@ public class FindEl {
         for (int i = 0; i < value.length; i++) {
             if (key.equals(value[i])) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
@@ -14,10 +15,41 @@ public class FindEl {
         return rsl;
     }
 
+    public static boolean sent(String value, String[] abuses) throws ElementNotFoundException {
+        boolean rsl = false;
+        for (int i = 0; i < abuses.length; i++) {
+            if (value.equals(abuses[i])) {
+                rsl = true;
+                break;
+            }
+        }
+        if (rsl) {
+            throw new ElementAbuseException("Element is forbidden!");
+        }
+        return true;
+
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         String[] vector = new String[]{"Winter", "Spring", "Summer", "Autumn"};
         try {
-            indexOf(vector,"Spring_");
+            indexOf(vector, "Spring_");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
